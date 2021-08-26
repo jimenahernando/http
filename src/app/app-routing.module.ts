5d5be6c73e51a71b17b2fdfbcb2c58e5907aff2e
jimena <jimenahernando@hotmail.com>
2021-08-26 17:21:45 +0200

    Creo rutas, componentes.:lipstick:estilo body

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DetallePostComponent } from './components/detalle-post/detalle-post.component';
+import { FormPostComponent } from './components/form-post/form-post.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'posts/:idPost', component: DetallePostComponent },
+  { path: 'newPost', component: FormPostComponent },
+  { path: 'updatePost/:idPost', component: FormPostComponent },
+  { path: '**', redirectTo: '/' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetallePostComponent } from './components/detalle-post/detalle-post.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormPostComponent } from './components/form-post/form-post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetallePostComponent,
+    HomeComponent,
+    FormPostComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
