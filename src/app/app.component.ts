d9106bf5ee2846c32a994a8050bdb3bf8ee68852
jimena <jimenahernando@hotmail.com>
2021-08-26 17:23:29 +0200

    Maqueto estructura transversal en appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Post } from './interfaces/post.interface';
+import { PostsService } from './services/posts.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'http';
+export class AppComponent implements OnInit {
+  
+  constructor(){
+  }
+
+  ngOnInit(): void {
+  } 
 }
